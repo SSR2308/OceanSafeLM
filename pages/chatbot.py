be0ffa5ad4daa7344d35ed9529e80322de1df178
SSR2308 <148827491+SSR2308@users.noreply.github.com>
2025-10-26 21:17:31 -0700

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -2,3 +2,13 @@ import streamlit as st
 from rag import response_generator 
 
 st.title("Ocean Safety Chatbot") 
+
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+for message in st.session_state.messages: 
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("What's up?"):
+  st.markdown("Hi") 
